8ad4e11c597cb1b9779601894b18511c4845b484
wy-luke <wykj.luke@gmail.com>
2024-02-25 02:48:16 +0800

    test: update tests

--- apps/vscode/src/test/extension.test.ts
+++ apps/vscode/src/test/extension.test.ts
@@ -13,8 +13,8 @@ suite('Extension Test Suite', () => {
   test('Check the default value of configs', () => {
     const config = vscode.workspace.getConfiguration('label-push')
     assert.strictEqual(config.get('label'), '[build]')
-    assert.strictEqual(config.get('commitEmpty'), false)
     assert.strictEqual(config.get('showStatusBarItem'), true)
+    assert.strictEqual(config.get('commitEmpty'), 'Suggest')
     assert.strictEqual(config.get('publishBranch'), 'Suggest')
     assert.strictEqual(config.get('publishDeletedBranch'), 'Suggest')
     assert.strictEqual(config.get('addStaged'), 'Suggest')
