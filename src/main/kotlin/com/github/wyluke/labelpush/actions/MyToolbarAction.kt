2d8cfffe88bdda9d87731a2c6fcb7fdcdcad44e7
wy-luke <wykj.luke@gmail.com>
2024-03-03 01:40:15 +0800

    feat: add an action and a notification :seedling:

--- src/main/kotlin/com/github/wyluke/labelpush/actions/MyToolbarAction.kt
+++ src/main/kotlin/com/github/wyluke/labelpush/actions/MyToolbarAction.kt
@@ -1,11 +1,27 @@
 package com.github.wyluke.labelpush.actions
 
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.DumbAware
+
 
 class MyToolbarAction : AnAction(), DumbAware {
     override fun actionPerformed(e: AnActionEvent) {
         // Handle button click here
+        thisLogger().info("Button clicked")
+
+        // create a notification
+        Notifications.Bus.notify(
+            Notification(
+                "Label Push Notifications",
+                "Label Push Notification",
+                "Button clicked",
+                NotificationType.WARNING
+            )
+        )
     }
 }
